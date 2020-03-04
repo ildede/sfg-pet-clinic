b75d62e2ab401a7345f94bf9ea6a76523d0208a9
Davide Pedron <davide@happydev.cloud>
2020-03-04 02:10:21 +0100

    Implement Map based service. Closes #6

---
+++ pet-clinic-data/src/main/java/cloud/happydev/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package cloud.happydev.sfgpetclinic.services.map;
+
+import cloud.happydev.sfgpetclinic.model.Pet;
+import cloud.happydev.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
