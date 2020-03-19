d989f8f1a2dd87b6b5438eb704f2443b50e40665
Davide Pedron <davide@happydev.cloud>
2020-03-19 01:44:07 +0100

    Bootstrap pettypes. Closes #32

--- pet-clinic-data/src/main/java/cloud/happydev/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/cloud/happydev/sfgpetclinic/model/PetType.java
@@ -1,4 +1,14 @@
 package cloud.happydev.sfgpetclinic.model;
 
 public class PetType extends BaseEntity {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
