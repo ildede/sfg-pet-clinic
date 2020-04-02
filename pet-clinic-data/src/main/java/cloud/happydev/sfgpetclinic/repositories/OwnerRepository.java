d1d514ef05908042820b6a1ac1ced5a388a0016b
Davide Pedron <davide@happydev.cloud>
2020-04-03 01:51:13 +0200

    Spring data jpa service for owner. Closes #40

--- pet-clinic-data/src/main/java/cloud/happydev/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/cloud/happydev/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import cloud.happydev.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
