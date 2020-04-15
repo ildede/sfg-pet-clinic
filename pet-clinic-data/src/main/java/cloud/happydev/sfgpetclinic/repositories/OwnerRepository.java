8ef824443b8dfe213a9bba89f56741ed4d486d84
Davide Pedron <davide@happydev.cloud>
2020-04-15 20:22:37 +0200

    Implement find owner. Close #54

--- pet-clinic-data/src/main/java/cloud/happydev/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/cloud/happydev/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,10 @@ package cloud.happydev.sfgpetclinic.repositories;
 import cloud.happydev.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/cloud/happydev/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/cloud/happydev/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package cloud.happydev.sfgpetclinic.services;
 
 import cloud.happydev.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
