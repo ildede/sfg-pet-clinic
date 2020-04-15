960f24825914d65a92c870bc2cb50add3ff1825d
Davide Pedron <davide@happydev.cloud>
2020-04-15 19:49:01 +0200

    Display owner record. Close #53

--- pet-clinic-web/src/test/java/cloud/happydev/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/cloud/happydev/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -13,7 +13,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -72,5 +73,14 @@ class OwnerControllerTest {
                 .andExpect(view().name("notimplemented"));
 
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwners() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
